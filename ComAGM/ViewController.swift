2a2453b6b0f872d5ff9c80a6f58bd88047466706
Your Name <you@example.com>
2018-11-15 11:04:46 +0300

    Initial Commit

---
+++ ComAGM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ComAGM
+//
+//  Created by Владимир Уланов on 15.11.2018.
+//  Copyright © 2018 UlanovApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
