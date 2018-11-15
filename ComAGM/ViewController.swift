9376aba1da6c062533669a4eb86386e1f87a5da5
Your Name <you@example.com>
2018-11-15 11:22:03 +0300

    //com1

--- ComAGM/ViewController.swift
+++ ComAGM/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+let comParametr = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
